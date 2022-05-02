2349828d5f9063d7ddf807c1d919f285bfe6a662
EdoardoCarlesi <gatto@nanowar.it>
2022-05-02 15:23:08 +0200

    app views class

--- advanced/advcbv/app/views.py
+++ advanced/advcbv/app/views.py
@@ -1,17 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView
+from django.views.generic import View, TemplateView, ListView, DetailView
 from django.http import HttpResponse
+from app import models
 
-# Simple example
-class CBViews(View):
-    def get(self, request):
-        return HttpResponse('FanCool')
 
-class IndexViews(TemplateView):
-    template_name = 'app/index.html'
+class SchoolView(ListView):
+    model = models.School
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['injection'] = 'BASIC_INJ'
-        return context
+class SchoolDetailView(DetailView):
+    model = models.School
+    template_name = 'app/school.html'
 
