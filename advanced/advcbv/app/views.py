6cf2800522bc969454ca53aec64dba3af17b28a0
Edoardo Carlesi <gatto@nanowar.it>
2022-05-04 14:55:43 +0200

    in app templates

--- advanced/advcbv/app/views.py
+++ advanced/advcbv/app/views.py
@@ -3,11 +3,13 @@ from django.views.generic import View, TemplateView, ListView, DetailView
 from django.http import HttpResponse
 from app import models
 
+class IndexView(TemplateView):
+    template_name = 'app/index.html'
 
-class SchoolView(ListView):
+class SchoolListView(ListView):
     model = models.School
+    context_object_name = 'schools'
 
 class SchoolDetailView(DetailView):
+    context_object_name = 'school_detail'
     model = models.School
-    template_name = 'app/school.html'
-
